1764a84a51c3f1976df7d41a153161218ab6f268
gerald <geraldsewoatsri@aol.com>
2024-10-31 13:10:17 -0700

    Added move method which is responsible for snake movement and create_snake method responsible for creating a 3 segment snake anytime an instance of Snake class is created

---
+++ snake.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
+
+
+class Snake:
+
+    def __init__(self):
+        self.snake_segments = []
+        self.create_snake()
+
+
+    def move(self):
+        for seg_num in range(len(self.snake_segments) - 1, 0, -1):
+            new_x = self.snake_segments[seg_num - 1].xcor()
+            new_y = self.snake_segments[seg_num - 1].ycor()
+            self.snake_segments[seg_num].goto(new_x, new_y)
+        self.snake_segments[0].forward(20)
+
+    def create_snake(self):
+        for position in STARTING_POSITIONS:
+            new_segment = Turtle("square")
+            new_segment.color("white")
+            new_segment.penup()
+            new_segment.goto(position)
+            self.snake_segments.append(new_segment)
